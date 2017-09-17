737ee952c419aed87aa9c050d645464e0181b881
flyhawksz <30296157+flyhawksz@users.noreply.github.com>
2017-09-17 18:17:45 +0800

    add Lstack and Sstack

---
+++ Class_LStack.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/17 18:07
+# @Author  : zhangqi
+# @Site    : 
+# @File    : Class_LStack.py
+# @Software: PyCharm Community Edition
+
+from Class_LNode2 import Node
+
+class StackUnderflow(ValueError):
+    pass
+
+
+class LStack():
+    """
+    基于连接表技术实现的栈类， 用LNode 作为节点
+    """
+    def __init__(self):
+        self._top = None
+
+    def is_empty(self):
+        return self._top is None
+
+    def top(self):
+        if self._top is None:
+            raise StackUnderflow("")
+        return self._top.elem
+
+    def push(self, elem):
+        self._top = Node(elem, self._top)
+
+    def pop(self):
+        if self._top is None:
+            raise StackUnderflow("")
+        p = self._top
+        self._top = p.next
+        return p.elem
---
+++ Class_SStack.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+
+class StackUnderflow(ValueError):
+    pass
+
+
+class SStack():
+    """
+    基于顺序表技术的栈类
+    采用list对象，_elems 存储栈中元素
+    所有栈操作映射到list操作
+    """
+
+    # noinspection InjectedReferences
+    def __init__(self):
+        """
+        生成一个空表
+        """
+        self._elems = []
+
+    def is_empty(self):
+        return self._elems == []
+
+    def top(self):
+        if self._elems == []:
+            raise StackUnderflow()
+        return self._elems[-1]
+
+    def push(self, elem):
+        self._elems.append(elem)
+
+    def pop(self):
+        if self._elems == []:
+            raise StackUnderflow('')
+        return self._elems.pop()
+
+
