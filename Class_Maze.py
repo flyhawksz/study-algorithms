2a6af1ca243ff1f1a02372b0bda39489fb6f9829
flyhawksz <30296157+flyhawksz@users.noreply.github.com>
2017-09-19 15:58:09 +0800

    add maze

---
+++ Class_Maze.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017-9-19 11:13
+# @Author  : zhangqi
+# @File    : Class_Maze.py
+# @Software: PyCharm Community Edition
+
+
+maze = [[1, 0, 0, 1, 0, 1],
+      [1, 1, 1, 0, 1, 0],
+      [0, 0, 1, 0, 1, 0],
+      [0, 1, 1, 1, 0, 0],
+      [0, 0, 0, 1, 0, 0],
+      [1, 0, 0, 0, 0, 0]]
