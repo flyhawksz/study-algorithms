3fa0c42865726e6b5f92f72b28215f1c10e62ceb
flyhawksz <30296157+flyhawksz@users.noreply.github.com>
2017-09-21 17:27:14 +0800

    edit fib and edit maze

--- Class_Maze.py
+++ Class_Maze.py
@@ -4,10 +4,38 @@
 # @File    : Class_Maze.py
 # @Software: PyCharm Community Edition
 
-
+# 四个方向，一步
+dirs = [(0, 1), (1,0), (0, -1), (-1, 0)]
 maze = [[1, 0, 0, 1, 0, 1],
       [1, 1, 1, 0, 1, 0],
       [0, 0, 1, 0, 1, 0],
       [0, 1, 1, 1, 0, 0],
       [0, 0, 0, 1, 0, 0],
       [1, 0, 0, 0, 0, 0]]
+
+
+
+def mark(maze, pos):
+	maze[pos[0]][pos[1]] = 2
+
+
+def passible(maze, pos):
+	return maze[pos[0]][pos[1]] == 0
+
+
+def find_path(maze, pos, end):
+	mark(maze, pos)
+	if pos == end:
+		print (pos, )
+		return True
+	for i in range(4):
+		nextp = pos[0] + dirs[i][0], pos[1] + dirs[i][1]
+		if passible(maze, nextp):
+			print (pos,)
+			return True
+
+	return False
+
+
+if __name__ == '__main__':
+	print(find_path(maze, (1, 3), (0, 2)))
--- Class_fib.py
+++ Class_fib.py
@@ -4,14 +4,42 @@
 # @File    : Class_fib.py
 # @Software: PyCharm Community Edition
 
+def fib21(i):
+    firstNumber = 0
+    secondNumber = 1
 
+    for l in range(i):
+        print firstNumber
+        # method 1
+        firstNumber, secondNumber = secondNumber, firstNumber + secondNumber
+        # 在这里需要注意，“i, j = i, i + j”这条代码不能写成如下所示：
+        # i = j
+        # j = i + j
+        # 如果写成这样，j就不是前两位相加的值，而是已经被j赋过值的i和j相加的值
+        # print firstNumber
+    # 返回某项
+    return firstNumber
 
-def fib2(i):
-    for j in range(i):
-        
+def fib22(i):
+    firstNumber = 0
+    secondNumber = 1
 
+    for l in range(i):
+        if l == 0:
+            fiboNumber = 0
+            print fiboNumber
+        elif l == 1:
+            fiboNumber = 1
+            print fiboNumber
+        else:
+            # method 2
+            fiboNumber = firstNumber + secondNumber
+            firstNumber = secondNumber
+            secondNumber = fiboNumber
+            print fiboNumber
 
-
+    print '-'*50
+    return fiboNumber
 
 
 def fib(i):
@@ -19,11 +47,10 @@ def fib(i):
     :param i:int number
     :return: one fibo number
     """
-    if i <= 2:
+    if i == 0 or i == 1:
         return i
     else:
         return fib(i-1) + fib(i - 2)
-
 
 
 if __name__ == '__main__':
@@ -35,8 +62,16 @@ if __name__ == '__main__':
         print("输入正数")
     else:
         print("斐波那契数列:")
-        print(fib(nterms - 1))
-        for i in range(nterms):
-            print(fib(i))
+        # test 1
+        # fib21(nterms)
+
+        # test 2
+        print(fib22(nterms))
+
+
+        # test3
+        # print(fib(nterms - 1))
+        # for i in range(nterms):
+        #     print(fib(i))
 
 
