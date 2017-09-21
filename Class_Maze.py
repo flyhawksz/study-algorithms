c0eaef26a2437dde72341cf3dd72a156aac3c40b
flyhawksz <30296157+flyhawksz@users.noreply.github.com>
2017-09-21 23:34:21 +0800

    edit maze

--- Class_Maze.py
+++ Class_Maze.py
@@ -5,14 +5,15 @@
 # @Software: PyCharm Community Edition
 
 # 四个方向，一步
-dirs = [(0, 1), (1,0), (0, -1), (-1, 0)]
-maze = [[1, 0, 0, 1, 0, 1],
-      [1, 1, 1, 0, 1, 0],
-      [0, 0, 1, 0, 1, 0],
-      [0, 1, 1, 1, 0, 0],
-      [0, 0, 0, 1, 0, 0],
-      [1, 0, 0, 0, 0, 0]]
-
+dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+maze = [[1, 1, 1, 1, 1, 1, 1, 1],
+		[1, 1, 0, 0, 1, 0, 1, 1],
+		[1, 1, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1]]
 
 
 def mark(maze, pos):
@@ -20,22 +21,26 @@ def mark(maze, pos):
 
 
 def passible(maze, pos):
-	return maze[pos[0]][pos[1]] == 0
+	if maze[pos[0]][pos[1]] == 0:
+		return True
+	else:
+		return False
 
 
 def find_path(maze, pos, end):
 	mark(maze, pos)
 	if pos == end:
-		print (pos, )
+		print (pos,)
 		return True
 	for i in range(4):
 		nextp = pos[0] + dirs[i][0], pos[1] + dirs[i][1]
 		if passible(maze, nextp):
-			print (pos,)
-			return True
+			if find_path(maze, nextp, end):
+				print (pos,)
+				return True
 
 	return False
 
 
 if __name__ == '__main__':
-	print(find_path(maze, (1, 3), (0, 2)))
+	print(find_path(maze, (1, 2), (3, 1)))
