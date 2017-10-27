67d3795ae5e8fd9d73e0d78c5b6b988f9f96fddb
flyhawksz <30296157+flyhawksz@users.noreply.github.com>
2017-10-27 17:16:50 +0800

    add question birary tree

---
+++ Question_BinaryTree.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017-10-27 16:45
+# @Author  : zhangqi
+# @File    : Question_BinaryTree.py
+# @Software: PyCharm Community Edition
+
+class binaryTree:
+	def __init__(self, rootObj):
+		self.key = rootObj
+		self.leftChild = None
+		self.rightChild = None
+		
+		
