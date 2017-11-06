0d2114947ceb131071e6d23c6fa342d1db6f4e29
flyhawksz <30296157+flyhawksz@users.noreply.github.com>
2017-11-06 17:31:37 +0800

    add graph matrix to study edit Class graph matrix

--- Class_GraphMatrix.py
+++ Class_GraphMatrix.py
@@ -14,17 +14,21 @@ from networkx.classes.graph import Graph
 
 class Graph_Matrix:
 
-	def __init__(self, vertices={}, matrix=[]):
+	def __init__(self, vertices=[], matrix=[]):
 		"""
 		
 		:param vertices:a dict with vertex id and index of matrix , such as {vertex:index}
 		:param matrix: a matrix
 		"""
 		self.matrix = matrix
-		self.numVertices = 0
-		self.numEdges = 0
-		# self.vertexList = self.getAllVertices()
-		# self.edgesList = self.getAllEdges()
+		self.edges = {}
+		if len(matrix) > 0:
+			if len(vertices) != len(matrix):
+				raise IndexError
+			self.edges = self.getAllEdges()
+			
+		self.numEdges = len(matrix)
+		self.vertices = vertices
 
 	def isOutRange(self, x):
 		try:
@@ -38,7 +42,10 @@ class Graph_Matrix:
 			self.numVertices = len(self.matrix)
 		return self.numVertices == 0
 
-	def addVertex(self):
+	def addVertex(self, key):
+		if key not in self.vertexList:
+			self.vertexList[key] = len(self.vertexList) + 1
+			
 		# add a vertex mean add a row and a column
 		# add a column for every row
 		for i in range(self.getVerticesNumbers()):
@@ -52,8 +59,23 @@ class Graph_Matrix:
 	def getVertex(self, key):
 		pass
 
-	def addEdge(self, fromV, toV, cost=0):
-		pass
+	def addEdge(self, tailV, headV, cost=0):
+		if self.vertexList.index(tailV) >= 0:
+			self.addVertex(tailV)
+		# if fromV not in self.vertexList:
+		# 	self.addVertex(fromV)
+		if self.vertexList.index(headV) >= 0:
+			self.addVertex(headV)
+		# if toV not in self.vertexList:
+		# 	self.addVertex(toV)
+		
+		# for directory matrix
+		self.matrix[self.vertexList.index(tailV)][self.vertexList.index(headV)] = cost
+		# for non-directory matrix
+		# self.matrix[self.vertexList.index(fromV)][self.vertexList.index(toV)] = \
+		# 	self.matrix[self.vertexList.index(toV)][self.vertexList.index(fromV)] = cost
+	
+		self.edgesList[(tailV, headV)] = cost
 
 	def getEdges(self, V):
 		pass
@@ -64,25 +86,33 @@ class Graph_Matrix:
 		return self.numVertices
 
 	def getAllVertices(self):
-		pass
+		return self.vertexList
 
 	def getAllEdges(self):
-		pass
+		for i in range(len(self.matrix)):
+			for j in range(len(self.matrix)):
+				if 0 < self.matrix[i][j] < float('inf'):
+					self.edgesList[self.vertexList[i], self.vertexList[j]] = self.matrix[i][j]
+					
+		return self.edgesList
 
-def createMatrix():
+
+
+
+def createList():
 	# 生成100个随机0,1之间的浮点数序列l
 	x = [random.random() for i in range(100)]
 	return x
 
 
-def createMatrix2():
+def createList2():
 	x = []
 	for i in range(100):
 		x.append(random.randint(0, 1))
 	return x
 
 
-def createMatrix3():
+def createList3():
 	n = input('please input n of the matrix')
 	x = []
 	for i in range(n):
@@ -90,11 +120,10 @@ def createMatrix3():
 	return x
 
 
-if __name__ == '__main__':
-	print (createMatrix())
-	print ('-' * 80)
-	print (createMatrix3())
+def createMatrix():
+
 	a, b, c, d, e, f, g, h = range(8)
+	
 	N = [[0, 1, 1, 1, 1, 1, 0, 0],  # a
 		 [0, 0, 1, 0, 1, 0, 0, 0],  # b
 		 [0, 0, 0, 1, 0, 0, 0, 0],  # c
@@ -107,8 +136,9 @@ if __name__ == '__main__':
 	# N[a][b]
 	# 1
 	# sum(N[f]))
+	
 
-
+def createMatrix2():
 	a, b, c, d, e, f, g, h = range(8)
 	inf = float('inf')
 	w = N = [[0, 2, 1, 3, 9, 4, inf, inf],  # a
@@ -125,3 +155,23 @@ if __name__ == '__main__':
 		# w[c][e]<inf
 		# False
 		# sum(1 for w in w[a] if w<inf)-1
+	
+	
+def createMatrix3():
+	vertex = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+	edge_list = [('A', 'F', 9), ('A', 'B', 10), ('A', 'G', 15), ('B', 'F', 2),
+	             ('G', 'F', 3), ('G', 'E', 12), ('G', 'C', 10), ('C', 'E', 1),
+	             ('E', 'D', 7)]
+	
+	
+
+
+if __name__ == '__main__':
+	print (createList())
+	print ('-' * 80)
+	print (createList3())
+
+
+
+	
+	
